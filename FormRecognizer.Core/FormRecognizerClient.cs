d5a54accf1ef31618053eac82b4ec6f93f83e96a
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-01-04 22:23:04 +0100

    added core project for document analyzing

---
+++ FormRecognizer.Core/FormRecognizerClient.cs
@@ -0,0 +1,122 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+
+namespace FormRecognizer.Core;
+
+public class FormRecognizerClient
+{
+    private readonly string endpoint = "Endpoint";
+    private readonly string apiKey = "ApiKey";
+    private readonly AzureKeyCredential credential;
+    private readonly DocumentAnalysisClient client;
+
+    public FormRecognizerClient()
+    {
+        credential = new AzureKeyCredential(apiKey!);
+        client = new DocumentAnalysisClient(new Uri(endpoint!), credential);
+    }
+
+    public async Task<AnalyzeResult> AnalyzeAsync()
+    {
+        // sample invoice document
+        // TODO: Take in image path as parameter
+
+        Uri invoiceUri = new Uri("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-invoice.pdf");
+
+        AnalyzeDocumentOperation operation = await client.StartAnalyzeDocumentFromUriAsync("prebuilt-invoice", invoiceUri);
+
+        await operation.WaitForCompletionAsync();
+
+        AnalyzeResult result = operation.Value;
+
+        for (int i = 0; i < result.Documents.Count; i++)
+        {
+            Console.WriteLine($"Document {i}:");
+
+            AnalyzedDocument document = result.Documents[i];
+
+            if (document.Fields.TryGetValue("VendorName", out DocumentField? vendorNameField))
+            {
+                if (vendorNameField.ValueType == DocumentFieldType.String)
+                {
+                    string vendorName = vendorNameField.AsString();
+                    Console.WriteLine($"Vendor Name: '{vendorName}', with confidence {vendorNameField.Confidence}");
+                }
+            }
+
+            if (document.Fields.TryGetValue("CustomerName", out DocumentField? customerNameField))
+            {
+                if (customerNameField.ValueType == DocumentFieldType.String)
+                {
+                    string customerName = customerNameField.AsString();
+                    Console.WriteLine($"Customer Name: '{customerName}', with confidence {customerNameField.Confidence}");
+                }
+            }
+
+            if (document.Fields.TryGetValue("Items", out DocumentField? itemsField))
+            {
+                if (itemsField.ValueType == DocumentFieldType.List)
+                {
+                    foreach (DocumentField itemField in itemsField.AsList())
+                    {
+                        Console.WriteLine("Item:");
+
+                        if (itemField.ValueType == DocumentFieldType.Dictionary)
+                        {
+                            IReadOnlyDictionary<string, DocumentField> itemFields = itemField.AsDictionary();
+
+                            if (itemFields.TryGetValue("Description", out DocumentField? itemDescriptionField))
+                            {
+                                if (itemDescriptionField.ValueType == DocumentFieldType.String)
+                                {
+                                    string itemDescription = itemDescriptionField.AsString();
+
+                                    Console.WriteLine($"  Description: '{itemDescription}', with confidence {itemDescriptionField.Confidence}");
+                                }
+                            }
+
+                            if (itemFields.TryGetValue("Amount", out DocumentField? itemAmountField))
+                            {
+                                if (itemAmountField.ValueType == DocumentFieldType.Double)
+                                {
+                                    double itemAmount = itemAmountField.AsDouble();
+
+                                    Console.WriteLine($"  Amount: '{itemAmount}', with confidence {itemAmountField.Confidence}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (document.Fields.TryGetValue("SubTotal", out DocumentField? subTotalField))
+            {
+                if (subTotalField.ValueType == DocumentFieldType.Double)
+                {
+                    double subTotal = subTotalField.AsDouble();
+                    Console.WriteLine($"Sub Total: '{subTotal}', with confidence {subTotalField.Confidence}");
+                }
+            }
+
+            if (document.Fields.TryGetValue("TotalTax", out DocumentField? totalTaxField))
+            {
+                if (totalTaxField.ValueType == DocumentFieldType.Double)
+                {
+                    double totalTax = totalTaxField.AsDouble();
+                    Console.WriteLine($"Total Tax: '{totalTax}', with confidence {totalTaxField.Confidence}");
+                }
+            }
+
+            if (document.Fields.TryGetValue("InvoiceTotal", out DocumentField? invoiceTotalField))
+            {
+                if (invoiceTotalField.ValueType == DocumentFieldType.Double)
+                {
+                    double invoiceTotal = invoiceTotalField.AsDouble();
+                    Console.WriteLine($"Invoice Total: '{invoiceTotal}', with confidence {invoiceTotalField.Confidence}");
+                }
+            }
+        }
+
+        return result;
+    }
+}

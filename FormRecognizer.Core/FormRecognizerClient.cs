b3d8507d7e60be5f4e5022b53b580dd136607773
Andreas Nesheim <andreas.nesheim@capgemini.com>
2022-01-05 21:37:18 +0100

    added comment

--- FormRecognizer.Core/FormRecognizerClient.cs
+++ FormRecognizer.Core/FormRecognizerClient.cs
@@ -21,6 +21,7 @@ namespace FormRecognizer.Core
 
         public async Task<AnalyzeResult> AnalyzeAsync(string path)
         {
+            // Doesn't seem to work with just a path. Needs to be a hosted URL to work...
             // sample invoice document
             Uri invoiceUri = new Uri(path);
 

3616fe76a73713fd6da5a912e61553d1d506128a
Andreas Nesheim <andreas.nesheim@capgemini.com>
2022-01-08 22:28:53 +0100

    more refactoring

--- FormRecognizer.Core/BlobUploader.cs
+++ FormRecognizer.Core/BlobUploader.cs
@@ -11,35 +11,15 @@ namespace FormRecognizer.Core
 
         public async Task<string> UploadSync(string fileName, string filePath)
         {
-            // Create a BlobServiceClient object which will be used to create a container client
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            var blobServiceClient = new BlobServiceClient(connectionString);
 
-            //Create a unique name for the container
-            string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+            var containerName = "formrecognizerblobs" + Guid.NewGuid().ToString();
 
-            // Create the container and return a container client object
             BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
 
-            // Get a reference to a blob
-            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+            var blobClient = containerClient.GetBlobClient(fileName);
 
-            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
-
-            // Upload data from the local file
             await blobClient.UploadAsync(filePath, true);
-
-            Console.WriteLine("Listing blobs...");
-
-            // List all blobs in the container
-            var blobs = containerClient.GetBlobs();
-            foreach (BlobItem blobItem in blobs)
-            {
-                Console.WriteLine("\t" + blobItem.Name);
-                Console.WriteLine("\t" + blobClient.Uri.AbsoluteUri);
-            }
-
-
-            Console.WriteLine("Done");
 
             return blobClient.Uri.AbsoluteUri;
         }
